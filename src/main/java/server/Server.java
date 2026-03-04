a9177e8428e8f4db29a81d4ce0f59935d91da700
LeonardoMattioli <leonardomattioli00@gmail.com>
2026-03-03 21:54:33 -0300

    feat: ler e imprimir requisição HTTP raw no console

--- src/main/java/server/Server.java
+++ src/main/java/server/Server.java
@@ -1,15 +1,19 @@
 package server;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Server {
+
     private final int port;
 
     public Server(int port) {
         this.port = port;
     }
+
     public void start() {
         try (ServerSocket serverSocket = new ServerSocket(port)) {
             System.out.println("Servidor rodando na porta " + port + "...");
@@ -18,10 +22,31 @@ public class Server {
                 Socket clientSocket = serverSocket.accept();
                 System.out.println("Nova conexão recebida de: " +
                         clientSocket.getInetAddress().getHostAddress());
+
+                lerRequisicao(clientSocket);
             }
 
         } catch (IOException e) {
             System.err.println("Erro ao iniciar o servidor: " + e.getMessage());
+        }
+    }
+
+    private void lerRequisicao(Socket clientSocket) {
+        try {
+            BufferedReader reader = new BufferedReader(
+                    new InputStreamReader(clientSocket.getInputStream())
+            );
+
+            String linha;
+            while ((linha = reader.readLine()) != null && !linha.isEmpty()) {
+                System.out.println(linha);
+            }
+
+            System.out.println("--- fim da requisição ---");
+            clientSocket.close();
+
+        } catch (IOException e) {
+            System.err.println("Erro ao ler requisição: " + e.getMessage());
         }
     }
 
